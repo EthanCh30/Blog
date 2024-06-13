14d9ef161902579e951feac740eea572fda876a7
chen <565306120@qq.com>
2024-06-14 02:22:08 +0930

    4 commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '伊のBlog',
+    description: 'Just playing around',
+
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Leetcode题解', link: '/Leetcode题解/' },
+            { text: 'Github', link: 'https://github.com/EthanCh30' },
+        ],
+
+        sidebar: 'auto'
+    }
+
+
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# 你好，陌生人
+
+这个网站是我用来记录自己Leetcode刷题答案的
+
+感兴趣可以点击右上角Leetcode题解
+
+![fd33cc7e7be71da11f66b6b9d77d1d3](https://s2.loli.net/2024/06/14/ezPqct6r3HICjdV.jpg)
+
